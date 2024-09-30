13319a6e8ca3332b3848bd61a91a4c0bb3ccfc8f
JUANCHO222 <juanmmi252@gmail.com>
2024-09-30 13:15:46 -0600

    Familiarizacion de la tecnologia

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { HelloWorldScreen } from './Screens/HelloWorldScreen';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <HelloWorldScreen/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ Screens/HelloWorldScreen.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export const HelloWorldScreen = () => {
+    return (
+        <View>
+            <Text>UWU</Text>
+        </View>
+    );
+}
+
+
